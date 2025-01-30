d71575f7079da0c19aa51dabc065522b51e01cf2
Durga Potnuru <vijaypotnuru123@gmail.com>
2025-01-31 01:48:28 +0530

    feat: Implement core functionality for Teamsync backend
    - Established the foundational structure for the backend application.
    - Integrated database configuration for seamless data management.
    - Developed initial API endpoints for user authentication and team management.
    - Ensured code quality with proper documentation and testing.

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import "dotenv/config";
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+import session from "cookie-session";
+import { config } from "./config/app.config";
+import connectDatabase from "./config/database.config";
+
+const app = express();
+const BASE_PATH = config.BASE_PATH;
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    name: "session",
+    keys: [config.SESSION_SECRET],
+    maxAge: 24 * 60 * 60 * 1000,
+    secure: config.NODE_ENV === "production",
+    httpOnly: true,
+    sameSite: "lax",
+  })
+);
+
+app.use(
+  cors({
+    origin: config.FRONTEND_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  console.log(req.url);
+  res.status(200).json({
+    message: "Hello World Server is running",
+  });
+});
+
+app.listen(config.PORT, async () => {
+  console.log(`Server is running on port ${config.PORT}`);
+  await connectDatabase();
+});
