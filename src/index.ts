9e06325bea392b9b9e0c2a2a69bf20d11744027b
Durga Potnuru <vijaypotnuru123@gmail.com>
2025-02-20 11:36:45 +0530

    feat: some changes

--- src/index.ts
+++ src/index.ts
@@ -51,12 +51,12 @@ app.use(
 app.get(
   `/`,
   asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-    // throw new BadRequestException(
-    //   "This is a bad request",
-    //   ErrorCodeEnum.AUTH_INVALID_TOKEN
-    // );
+    throw new BadRequestException(
+      "This is a bad request",
+      ErrorCodeEnum.AUTH_INVALID_TOKEN
+    );
     return res.status(HTTPSTATUS.OK).json({
-      message: "Hello Welcome to Teamsync",
+      message: "Hello World Teamsync",
     });
   })
 );
@@ -71,8 +71,6 @@ app.use(`${BASE_PATH}/task`, isAuthenticated, taskRoutes);
 app.use(errorHandler);
 
 app.listen(config.PORT, async () => {
-  console.log(
-    `Server listening on port http://localhost:${config.PORT} in ${config.NODE_ENV}`
-  );
+  console.log(`Server listening on port ${config.PORT} in ${config.NODE_ENV}`);
   await connectDatabase();
 });
--- src/utils/get-env.ts
+++ src/utils/get-env.ts
@@ -1,6 +1,5 @@
 export const getEnv = (key: string, defaultValue: string = ""): string => {
   const value = process.env[key];
-  // console.log("key", key, "value", value, "defaultValue", defaultValue);
   if (value === undefined) {
     if (defaultValue) {
       return defaultValue;
